2d18130f737b8c7a2eeb0e9b06b52fefa5fe6ddb
Saurav <sauravc41@gmail.com>
2023-09-15 01:05:43 -0700

    add error testing

--- dig_test.go
+++ dig_test.go
@@ -79,8 +79,8 @@ func TestDig(t *testing.T) {
 	}
 }
 
-// TestDigStringReturn calls dig.Dig with a json document loaded as a interface{} and keys
-// which are the path to a string, checking for the correct return value and no error.
+// TestDigStringReturn calls dig.Dig with a json document loaded as a interface{} and a key
+// which is the path to a string, checking for the correct return value and no error.
 func TestDigStringReturn(t *testing.T) {
 	json := returnInterface()
 
@@ -92,8 +92,8 @@ func TestDigStringReturn(t *testing.T) {
 	}
 }
 
-// TestDigFloatReturn calls dig.Dig with a json document loaded as a interface{} and keys which
-// are the path to a floating point number, checking for the correct return value and no error.
+// TestDigFloatReturn calls dig.Dig with a json document loaded as a interface{} and a key which
+// is the path to a floating point number, checking for the correct return value and no error.
 func TestDigFloatReturn(t *testing.T) {
 	json := returnInterface()
 
@@ -102,5 +102,77 @@ func TestDigFloatReturn(t *testing.T) {
 
 	if got != want || err != nil {
 		t.Errorf("got %v, %v - wanted %v, %v", got, err, want, nil)
+	}
+}
+
+// TestDigErrorEmpty calls dig.Dig with a json document loaded as a
+// interface{} and empty keys, checking for an error.
+func TestDigErrorEmptyKey(t *testing.T) {
+	json := returnInterface()
+
+	got, err := Dig(json)
+
+	if got != nil || err == nil {
+		t.Errorf("got %v, %v - wanted %v, %v", got, err, nil, "key is missing")
+	}
+}
+
+// TestDigErrorKeyNotFound calls dig.Dig with a json document loaded as a interface{}
+// and keys that lead to a value that does not exist, checking for an error.
+func TestDigErrorKeyNotFound(t *testing.T) {
+	json := returnInterface()
+
+	got, err := Dig(json, "menu", "apples")
+
+	if got != nil || err == nil {
+		t.Errorf("got %v, %v - wanted %v, %v", got, err, nil, "key not found")
+	}
+}
+
+// TestDigErrorUnsupportedKey calls dig.Dig with a json document loaded as a interface{}
+// and a float 1.0 as a key parameter which is not supported, checking for an error.
+func TestDigErrorUnsupportedKey(t *testing.T) {
+	json := returnInterface()
+
+	got, err := Dig(json, "menu", "items", 1.0, "id")
+
+	if got != nil || err == nil {
+		t.Errorf("got %v, %v - wanted %v, %v", got, err, nil, "key is not supported")
+	}
+}
+
+// TestDigErrorNotSlice calls dig.Dig with a json document loaded as a interface{} and
+// keys that try to access a string map with an int 2 parameter, checking for an error.
+func TestDigErrorNotSlice(t *testing.T) {
+	json := returnInterface()
+
+	got, err := Dig(json, "menu", 2, "id")
+
+	if got != nil || err == nil {
+		t.Errorf("got %v, %v - wanted %v, %v", got, err, nil, "not a int accessable map")
+	}
+}
+
+// TestDigErrorNoStringAccess calls dig.Dig with a json document loaded as a interface{} and
+// keys that try to access a int map/array with an string 'id' parameter, checking for an error.
+func TestDigErrorNoStringAccess(t *testing.T) {
+	json := returnInterface()
+
+	got, err := Dig(json, "menu", "items", "id")
+
+	if got != nil || err == nil {
+		t.Errorf("got %v, %v - wanted %v, %v", got, err, nil, "not a string accessable map")
+	}
+}
+
+// TestDigErrorIndexOutOfRange calls dig.Dig with a json document loaded as a interface{}
+// and keys that try to access a int map element that is out of range, checking for an error.
+func TestDigErrorIndexOutOfRange(t *testing.T) {
+	json := returnInterface()
+
+	got, err := Dig(json, "menu", "items", 6)
+
+	if got != nil || err == nil {
+		t.Errorf("got %v, %v - wanted %v, %v", got, err, nil, "index out of range")
 	}
 }
